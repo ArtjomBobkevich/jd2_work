748b171fdaa4f891a1df5ca277e1b38a35df09f2
ArtjomBobkevich <wassax@yandex.ru>
2019-05-10 23:30:15 +0300

    changing my project

--- project-structure/database/src/main/java/by/itacademy/database/entity/Person.java
+++ project-structure/database/src/main/java/by/itacademy/database/entity/Person.java
@@ -1,22 +1,36 @@
 package by.itacademy.database.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//@AllArgsConstructor
 @Builder
 @Accessors(chain = true)
+@Entity
+@Table (name = "person", schema = "flea_market")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String avatar;
     private String login;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String age;
     private String mail;
