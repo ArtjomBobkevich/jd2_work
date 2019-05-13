c0a45e65a0f4e289353e50299d732ab4b7581ca4
ArtjomBobkevich <wassax@yandex.ru>
2019-05-13 15:56:44 +0300

    propiertes for test

--- project-structure/database/src/main/java/by/itacademy/database/entity/PersonRole.java
+++ project-structure/database/src/main/java/by/itacademy/database/entity/PersonRole.java
@@ -1,16 +1,27 @@
 package by.itacademy.database.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Data
-@AllArgsConstructor
+import javax.persistence.*;
+
+
+//@AllArgsConstructor
 @Builder
 @Accessors(chain = true)
+@Entity
+@Table(name = "person_role", schema = "flea_market")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class PersonRole {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "role")
     private String nameOfRole;
 }
---
+++ project-structure/database/src/test/java/by/itacademy/database/entity/PersonRoleTest.java
@@ -0,0 +1,7 @@
+package by.itacademy.database.entity;
+
+import static org.junit.Assert.*;
+
+public class PersonRoleTest {
+
+}
