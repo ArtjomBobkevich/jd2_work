b9730d3baa52767a3edcb0ee96a8296c58639fdf
ArtjomBobkevich <wassax@yandex.ru>
2019-06-10 11:26:16 +0300

    chtck

--- project-structure/web/src/main/java/com/itacademy/web/util/Context.java
+++ project-structure/web/src/main/java/com/itacademy/web/util/Context.java
@@ -1,14 +1,11 @@
 package com.itacademy.web.util;
 
-import com.itacademy.service.config.ServiceConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//public class Context {
 
-public class Context {
+//    private static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ServiceConfig.class);
 
-    private static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ServiceConfig.class);
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
-}
+//
+//    public static ApplicationContext getApplicationContext() {
+//        return applicationContext;
+//    }
+//}
